ec4c6b6e41a7fefac2ca77ebade27bdf0c69438c
gervasioartur <gervasioartur@outlook.com.br>
2023-11-05 05:52:23 -0300

    feat: ensure ProductController returns all products

--- product-service/src/main/java/com/online/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/online/productservice/controller/ProductController.java
@@ -1,10 +1,14 @@
 package com.online.productservice.controller;
 
 import com.online.productservice.dto.ProductRequest;
+import com.online.productservice.dto.ProductResponse;
+import com.online.productservice.model.Product;
 import com.online.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +20,22 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest request) {
         service.createProduct(request);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getProducts() {
+       List<Product> list = this.service.getAllProducts();
+       //map(product -> this.mapToProductResponse(product)).toList(); -> other way
+      return list.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product){
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .description(product.getDescription())
+                .build();
     }
 }
