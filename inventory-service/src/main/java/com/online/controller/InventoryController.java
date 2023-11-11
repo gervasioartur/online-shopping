340fa7400e1acef01d63312767d646c6cb086365
gervasioartur <gervasioartur@outlook.com.br>
2023-11-11 12:48:23 -0300

    chore: create inventory  controller

---
+++ inventory-service/src/main/java/com/online/controller/InventoryController.java
@@ -0,0 +1,18 @@
+package com.online.controller;
+
+import com.online.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+    private final InventoryService service;
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isnInStock(@PathVariable("sku-code") String skuCode) {
+        return this.service.isInStock(skuCode);
+    }
+}
