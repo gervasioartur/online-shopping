d715325707e7ed686f61cf8ae422116301a7c92a
gervasioartur <gervasioartur@outlook.com.br>
2023-11-11 12:40:46 -0300

    chore: creating order repository an order entity

---
+++ order-service/src/main/java/com/online/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.online.repository;
+
+import com.online.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
