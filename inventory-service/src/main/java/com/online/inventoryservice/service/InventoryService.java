81ae742f8b3a5e217dd3f2340763d2781d008a66
gervasioartur <gervasioartur@outlook.com.br>
2023-11-23 21:07:19 -0300

    feat: add Sl4j log to visualize te timeout while the request is in thead

--- inventory-service/src/main/java/com/online/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/online/inventoryservice/service/InventoryService.java
@@ -3,18 +3,24 @@ package com.online.inventoryservice.service;
 import com.online.inventoryservice.dto.InventoryResponse;
 import com.online.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class InventoryService {
     private final InventoryRepository repository;
 
+    @SneakyThrows
     @Transactional(readOnly = true)
     public List<InventoryResponse> isInStock(List<String> skuCodes) {
+        log.info("Wait started");
+        Thread.sleep(10000);
+        log.info("Wait ended");
         return this.repository.findBySkuCodeIn(skuCodes)
                 .stream()
                 .map(inventory ->
